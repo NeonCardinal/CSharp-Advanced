8792eb6b9fc2e442d43f2d1525e0a9470e17d636
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-19 01:03:04 +0300

    add

---
+++ 001.StacksAndQueuesLab/006.Supermarket/Supermarket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Supermarket
+{
+    static void Main()
+    {
+        Queue<string> names = new Queue<string>();
+
+        while(true)
+        {
+            string cmd = Console.ReadLine();
+
+            if(cmd == "Paid")
+            {
+                foreach(var item in names)
+                {
+                    Console.WriteLine(item);
+                }
+
+                names.Clear();
+            }
+            else if(cmd == "End")
+            {
+                Console.WriteLine($"{names.Count} people remaining.");
+                break;
+            }
+            else
+            {
+                names.Enqueue(cmd);
+            }
+        }
+    }
+}
