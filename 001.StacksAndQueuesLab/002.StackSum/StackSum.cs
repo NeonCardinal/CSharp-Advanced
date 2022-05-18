613b6fc07ee6daadc395e4f0a7185b115d5c8fb8
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-19 01:02:02 +0300

    add

---
+++ 001.StacksAndQueuesLab/002.StackSum/StackSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StackSum
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Stack<int> stack = new Stack<int>(array);
+
+        while(true)
+        {
+            string cmd = Console.ReadLine().ToLower();
+            string[] tokens = cmd.Split();
+
+            if(tokens[0] == "end")
+            {
+                break;
+            }
+            else if(tokens[0] == "add")
+            {
+                stack.Push(int.Parse(tokens[1]));
+                stack.Push(int.Parse(tokens[2]));
+            }
+            else if(tokens[0] == "remove")
+            {
+                int len = int.Parse(tokens[1]);
+                if(len < stack.Count)
+                {
+                    for(int i = 0; i < len; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(stack.Sum());
+    }
+}
