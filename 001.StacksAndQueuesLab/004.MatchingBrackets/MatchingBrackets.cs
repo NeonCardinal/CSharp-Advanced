81ef5e4a40820f2828570cc2350372e4eac43a2d
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-19 01:02:33 +0300

    add

---
+++ 001.StacksAndQueuesLab/004.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MatchingBrackets
+{
+    static void Main()
+    {
+        string exp = Console.ReadLine();
+        Stack<int> stack = new Stack<int>();
+
+        for(int i = 0; i < exp.Length; i++)
+        {
+            char ch = exp[i];
+
+            if(ch == '(')
+            {
+                stack.Push(i);
+            }
+            else if(ch == ')')
+            {
+                int startIndex = stack.Pop();
+                int endIndex = i;
+                string subExp = exp.Substring(startIndex, endIndex - startIndex + 1);
+                Console.WriteLine(subExp);
+            }
+        }
+    }
+}
