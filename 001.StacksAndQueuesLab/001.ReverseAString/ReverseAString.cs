d31e964e558eb681bfce1bb27c438823de622a9a
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-19 01:01:02 +0300

    add

---
+++ 001.StacksAndQueuesLab/001.ReverseAString/ReverseAString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ReverseAString
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        Stack<char> stack = new Stack<char>();
+
+        for(int i = 0; i < str.Length; i++)
+        {
+            stack.Push(str[i]);
+        }
+
+        Console.WriteLine(string.Join("", stack));
+    }
+}
