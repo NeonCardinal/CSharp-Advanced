01bd15b700f604e912adf3daeb386b014bf5b88a
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-26 22:45:10 +0300

    add

---
+++ 002.StacksAndQueuesExercise/004.FastFood/FastFood.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FastFood
+{
+    static void Main()
+    {
+        int quantityFood = int.Parse(Console.ReadLine());
+        List<int> ordersList = Console.ReadLine().Split().Select(int.Parse).ToList();
+        Queue<int> queue = new Queue<int>(ordersList);
+
+        Console.WriteLine(queue.Max());
+
+        int queueLen = queue.Count;
+        for(int i = 1; i <= queueLen; i++)
+        {
+            if(quantityFood >= queue.Peek())
+            {
+                quantityFood -= queue.Peek();
+                queue.Dequeue();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if(queue.Count == 0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.Write("Orders left: ");
+            Console.WriteLine(String.Join(" ", queue));
+        }
+    }
+}
