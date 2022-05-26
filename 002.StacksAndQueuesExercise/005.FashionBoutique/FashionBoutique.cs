268b72bda367cca497121a457b1e167914e27571
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-26 22:45:38 +0300

    add

---
+++ 002.StacksAndQueuesExercise/005.FashionBoutique/FashionBoutique.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class FashionBoutique
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int rackCapacity = int.Parse(Console.ReadLine());
+
+        Stack<int> stack = new Stack<int>(numbers);
+
+        int racksCounter = 1;
+        int sum = 0;
+
+        while(stack.Count > 0)
+        {
+            int curElement = stack.Pop();
+
+            if(sum + curElement <= rackCapacity)
+            {
+                sum += curElement;
+            }
+            else
+            {
+                sum = curElement;
+                racksCounter++;
+            }
+        }
+
+        Console.WriteLine(racksCounter);
+    }
+}
