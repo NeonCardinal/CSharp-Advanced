1460a0b2f483b1e070db25430cfd314309a1cd05
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 19:10:10 +0300

    add

---
+++ 003.MultidimensionalArraysLab/001.SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+public class SumMatrixElements
+{
+    static void Main()
+    {
+        int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int[,] matrix = new int[size[0], size[1]];
+
+        for(int i = 0; i < size[0]; i++)
+        {
+            int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            for(int j = 0; j < size[1]; j++)
+            {
+                matrix[i, j] = line[j];
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[i, j];
+            }
+        }
+
+        Console.WriteLine(matrix.GetLength(0));
+        Console.WriteLine(matrix.GetLength(1));
+        Console.WriteLine(sum);
+    }
+}
