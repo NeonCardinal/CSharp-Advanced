0965fe913c44c3a76a29db015bdb5eb4cccaf6c0
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 19:10:56 +0300

    add

---
+++ 003.MultidimensionalArraysLab/003.PrimaryDiagonal/PrimaryDiagonal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+public class PrimaryDiagonal
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = numbers[j];
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            sum += matrix[i, i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
