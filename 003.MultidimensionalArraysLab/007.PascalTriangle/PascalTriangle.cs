21c9f0dd3ac8a5f9255340591e469b1c702c3360
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 19:12:45 +0300

    add

---
+++ 003.MultidimensionalArraysLab/007.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class PascalTriangle
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[][] triangle = new long[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            triangle[i] = new long[i + 1];
+            triangle[i][0] = 1;
+
+            for(int j = 1; j < i; j++)
+            {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+
+            triangle[i][i] = 1;
+        }
+
+        for (int i = 0; i < triangle.Length; i++)
+        {
+            Console.WriteLine(string.Join(" ", triangle[i]));
+        }
+    }
+}
