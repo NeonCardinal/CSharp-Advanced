c4575e6e8badd8d26bfac5b4626c38221a1c72fc
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 12:55:55 +0300

    add

---
+++ 002.StacksAndQueuesExercise/009.SimpleTextEditor/SimpleTextEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class SimpleTextEditor
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Stack<string> stack = new Stack<string>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++)
+        {
+            string command = Console.ReadLine();
+
+            if(command.StartsWith("1"))
+            {
+                stack.Push(sb.ToString());
+                string text = command.Split()[1];
+                sb.Append(text);
+            }
+            else if(command.StartsWith("2"))
+            {
+                stack.Push(sb.ToString());
+                int count = int.Parse(command.Split()[1]);
+                sb.Remove(sb.Length - count, count);
+            }
+            else if(command.StartsWith("3"))
+            {
+                int index = int.Parse(command.Split()[1]);
+                Console.WriteLine(sb[index - 1]);
+            }
+            else if(command.StartsWith("4"))
+            {
+                string text = stack.Pop();
+                sb = new StringBuilder(text);
+            }
+        }
+    }
+}
