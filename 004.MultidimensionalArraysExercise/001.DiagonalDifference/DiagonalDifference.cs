a39fad26e408fd2dc92c5d7afba64ae508506542
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-28 21:37:52 +0300

    add

---
+++ 004.MultidimensionalArraysExercise/001.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+public class DiagonalDifference
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+
+        ReadMatrix(matrix, n);
+
+        int primSum = 0;
+        int secSum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            primSum += matrix[i, i];
+            secSum += matrix[i, n - 1 - i];
+        }
+
+        Console.WriteLine(Math.Abs(primSum - secSum));
+    }
+
+    static void ReadMatrix(int[,] matrix, int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for(int j = 0; j < size; j++)
+            {
+                matrix[i, j] = input[j];
+            }
+        }
+    }
+}
