8f0afea2fd5f5688cacff213499e904313fa6890
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-04 10:27:23 +0300

    add

---
+++ 005.SetsAndDictionariesAdvancedLab/001.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountSameValuesInArray
+{
+    static void Main()
+    {
+        Dictionary<double, int> counter = new Dictionary<double, int>();
+
+        double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+        foreach (var item in input)
+        {
+            if (!counter.ContainsKey(item))
+            {
+                counter[item] = 1;
+            }
+            else
+            {
+                counter[item]++;
+            }
+        }
+
+        foreach (var item in counter)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value} times");
+        }
+    }
+}
