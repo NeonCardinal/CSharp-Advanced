c1eed3eb4ff4b7e487c37e21f1761ba37a93b250
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-04 10:27:59 +0300

    add

---
+++ 005.SetsAndDictionariesAdvancedLab/003.LargestThreeNumbers/LargestThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LargestThreeNumbers
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] sortedNumber = numbers.OrderByDescending(x => x).ToArray();
+
+        for(int i = 0; i < sortedNumber.Length; i++)
+        {
+            if(i > 2)
+            {
+                break;
+            }
+
+            Console.Write($"{sortedNumber[i]} ");
+        }
+    }
+}
