79101341b4681855c58b5a4f3bc54138f0e0d63e
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-26 17:01:04 +0300

    add

---
+++ 005.SetsAndDictionariesAdvancedLab/007.ParkingLot/ParkingLot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ParkingLot
+{
+    static void Main()
+    {
+        HashSet<string> parking = new HashSet<string>();
+
+        string command = Console.ReadLine();
+
+        while(command != "END")
+        {
+            string[] tokens = command.Split(", ");
+            string cmd = tokens[0];
+            string carNumber = tokens[1];
+
+            if(cmd == "IN")
+            {
+                parking.Add(carNumber);
+            }
+            else if(cmd == "OUT")
+            {
+                parking.Remove(carNumber);
+            }
+
+            command = Console.ReadLine();
+        }
+
+        if(parking.Count != 0)
+        {
+            foreach (var item in parking)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
