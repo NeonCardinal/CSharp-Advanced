7b9d6d69e1f332715528db69a7d8a3015ce1ba23
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-27 00:11:34 +0300

    add

---
+++ 006.SetsAndDictionariesAdvancedExercise/003.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PeriodicTable
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        SortedSet<string> chElements = new SortedSet<string>();
+
+        for(int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            string[] elements = input.Split();
+
+            foreach(var element in elements)
+            {
+                chElements.Add(element);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", chElements));
+    }
+}
