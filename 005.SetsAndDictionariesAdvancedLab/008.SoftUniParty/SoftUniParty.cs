8d0e19f5051be329dde35c4d7be74ad38b21e37c
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-26 17:01:31 +0300

    add

---
+++ 005.SetsAndDictionariesAdvancedLab/008.SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SoftUniParty
+{
+    static void Main()
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        while(true)
+        {
+            string input = Console.ReadLine();
+
+            if(input == "PARTY")
+            {
+                break;
+            }
+
+            set.Add(input);
+        }
+
+        while(true)
+        {
+            string input = Console.ReadLine();
+
+            if(input == "END")
+            {
+                break;
+            }
+
+            if(set.Contains(input))
+            {
+                set.Remove(input);
+            }
+        }
+
+        Console.WriteLine(set.Count);
+
+        foreach(var item in set)
+        {
+            if (char.IsDigit(item[0]))
+            {
+                Console.WriteLine(item);
+            }
+            //else
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        foreach(var item in set)
+        {
+            if (char.IsLetter(item[0]))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
