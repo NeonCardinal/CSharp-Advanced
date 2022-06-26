fa98ea0cead959bd0ea3810b48bdbfc2bfd57bd8
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-27 00:11:13 +0300

    add

---
+++ 006.SetsAndDictionariesAdvancedExercise/002.SetsOfElements/SetsOfElements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SetsOfElements
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int n = int.Parse(input.Split()[0]);
+        int m = int.Parse(input.Split()[1]);
+
+        HashSet<int> firstSet = new HashSet<int>();
+        HashSet<int> secondSet = new HashSet<int>();
+
+        for(int i = 0; i < n; i++)
+        {
+            firstSet.Add(int.Parse(Console.ReadLine()));
+        }
+        
+        for(int i = 0; i < m; i++)
+        {
+            secondSet.Add(int.Parse(Console.ReadLine()));
+        }
+
+        firstSet.IntersectWith(secondSet);
+
+        Console.WriteLine(string.Join(" ", firstSet));
+    }
+}
