026c3579c9b7ae1b90de6b34ffd9580cdbb8c463
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-26 18:48:24 +0300

    add

---
+++ 007.StreamsFilesAndDirectoriesLab/002.LineNumbers/LineNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\Files\input.txt";
+            string outputFile = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputFile, outputFile);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFile, string outputFile)
+        {
+            StreamReader reader = new StreamReader(inputFile);
+            StreamWriter writer = new StreamWriter(outputFile);
+
+            using (reader)
+            {
+                int lineCounter = 1;
+                string line = reader.ReadLine();
+
+                while(line != null)
+                {
+                    writer.WriteLine(lineCounter + ". " + line);
+                    line = reader.ReadLine();
+                    lineCounter++;
+                }
+
+                //writer.Flush();
+                writer.Close();
+            }
+        }
+    }
+}
