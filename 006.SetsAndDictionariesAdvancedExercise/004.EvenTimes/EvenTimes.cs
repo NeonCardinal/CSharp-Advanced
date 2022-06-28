cadff5937ecb1e2b46eb92f472cb9b52b2014586
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-28 22:52:25 +0300

    add

---
+++ 006.SetsAndDictionariesAdvancedExercise/004.EvenTimes/EvenTimes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EvenTimes
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for(int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(!dict.ContainsKey(number))
+            {
+                dict.Add(number, 1);
+            }
+            else
+            {
+                dict[number]++;
+            }
+        }
+
+        Console.WriteLine(dict.First(x => x.Value % 2 == 0).Key);
+    }
+}
