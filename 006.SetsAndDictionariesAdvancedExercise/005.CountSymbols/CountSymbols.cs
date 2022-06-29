11d69199c57db58aea7c93a94ce253268a564979
NeonCardinal <nvnnaidenov@gmail.com>
2022-06-29 11:06:42 +0300

    add

---
+++ 006.SetsAndDictionariesAdvancedExercise/005.CountSymbols/CountSymbols.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CountSymbols
+{
+    static void Main()
+    {
+        string text = Console.ReadLine();
+        SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            if (!dict.ContainsKey(text[i]))
+            {
+                dict.Add(text[i], 1);
+            }
+            else
+            {
+                dict[text[i]]++;
+            }
+        }
+
+        foreach(var item in dict)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value} time/s");
+        }
+    }
+}
